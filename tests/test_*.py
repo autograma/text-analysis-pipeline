4a3c06a56d6aaf800342f881aec952720f58689a
autograma <luciano.r.tedin@icloud.com>
2026-05-06 22:31:36 +0000

    Update test_*.py

--- tests/test_*.py
+++ tests/test_*.py
@@ -1,3 +1,3 @@
 def test_path(path)
-if not path.exists():
-    raise FileNotFoundError(f"File was not found: {path}")
+    if not path.exists():
+        raise FileNotFoundError(f"File was not found: {path}")
